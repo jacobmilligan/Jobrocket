609d72da6ad21724ccd42395599734e61d9f5314
Jacob Milligan <jacobpmilligan@gmail.com>
2018-01-04 15:01:49 +1100

    docs/publish: Update publish script

--- Docs/publish.py
+++ Docs/publish.py
@@ -5,43 +5,38 @@ import os
 
 HTML_DIR = 'Generated/html'
 
+def print_status(status):
+    GREEN = '\033[32m'
+    WHITE = '\033[0m'
+    print('{0}{1}{2}'.format(GREEN, status, WHITE))
+
 def publish():
+    print_status("Jobrocket: Generating docs")
+
     cmd = subprocess.Popen(['git', 'status', '-s'], stdin=subprocess.PIPE,
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     result, err = cmd.communicate()
     output = result.decode('utf-8')
+
     if output:
-        print("The working directory is dirty. Please commit any pending changes.")
+        print_status("The working directory is dirty. Please commit any pending changes.")
         return
 
-    output_dir = os.path.join(os.getcwd(), HTML_DIR)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    output_dir = os.path.join(script_dir, HTML_DIR)
 
-    print("Checking out gh-pages branch into public")
-    subprocess.call(['git', 'worktree', 'add', '-B', 'gh-pages', output_dir, 'origin/gh-pages'])
+    print_status("Checking out gh-pages branch into public")
+    subprocess.call(['git', 'worktree', 'add', '-B', 'gh-pages', HTML_DIR, 'origin/gh-pages'], cwd=script_dir)
 
-    print('Generating HTML')
-    subprocess.call(['doxygen', 'Doxyfile'])
+    print_status('Generating HTML')
+    subprocess.call(['doxygen', 'Doxyfile'], cwd=script_dir)
 
-    print('Updating gh-pages branch')
-    subprocess.call(['git', 'add', '--all', '&&', 'git', 'commit', '-m',
-                     '"Publishing to gh-pages (publish.sh)"'], cwd=output_dir)
+    print_status('Updating gh-pages branch')
+    subprocess.call(['git', 'add', '--all'], cwd=output_dir)
+    subprocess.call(['git', 'commit', '-m','"docs/API: Update API reference docs and HTML"'], cwd=output_dir)
+    subprocess.call(['git', 'push', 'origin', 'gh-pages'])
 
 
 
 if __name__ == '__main__':
     publish()
-
-# if [[ $(git status -s) ]]
-# then
-#     echo "The working directory is dirty. Please commit any pending changes."
-#     exit 1;
-# fi
-
-# echo "Checking out gh-pages branch into public"
-# git worktree add -B gh-pages Generated/html origin/gh-pages
-
-# echo "Generating HTML"
-# doxygen Doxyfile
-
-# echo "Updating gh-pages branch"
-# cd Generated/html && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
